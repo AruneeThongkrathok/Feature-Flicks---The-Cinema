0cf8d2440411a187478e5b548d52a89c61b3524d
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-08-25 01:04:27 +0200

    added a route to BookMovie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import '../css/Movie.css';
+import React from "react";
+import { Card } from "react-bootstrap";
+import "../css/Movie.css";
 
 export const Movie = (props) => {
   const { title, description, date } = props;
   const { posterImage, length, categories } = description;
-  const posterImageURL = 'https://cinema-rest.nodehill.se/' + posterImage;
+  const posterImageURL = "https://cinema-rest.nodehill.se/" + posterImage;
 
   return (
     <Card className="movie-card">
       <div className="card-content">
         <div className="poster-container">
-          <Card.Img className="movie-poster" variant="top" src={posterImageURL} />
+          <Card.Img
+            className="movie-poster"
+            variant="top"
+            src={posterImageURL}
+          />
         </div>
         <div className="movie-details">
           <Card.Body>
             <Card.Title className="movie-title">{title}</Card.Title>
-            <Card.Text className="movie-info">Length: {length} minutes</Card.Text>
-            <Card.Text className="movie-info">Categories: {categories.join(', ')}</Card.Text>
+            <Card.Text className="movie-info">
+              Length: {length} minutes
+            </Card.Text>
+            <Card.Text className="movie-info">
+              Categories: {categories.join(", ")}
+            </Card.Text>
           </Card.Body>
         </div>
       </div>
