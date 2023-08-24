bd0d3e30ce59616235ee72a708c1bb6d79e98036
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2023-08-24 16:11:04 +0200

    improved the design of movie Cards

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import '../css/Movie.css';
 
 export const Movie = (props) => {
-  let { title, description, date} = props;
-  let { posterImage, length, categories} = description;
-
-  posterImage = 'https://cinema-rest.nodehill.se/' + posterImage;
+  const { title, description, date } = props;
+  const { posterImage, length, categories } = description;
+  const posterImageURL = 'https://cinema-rest.nodehill.se/' + posterImage;
 
   return (
-    <Card style={{ width: '20rem', background: 'white', color: 'black' }}>
-      <Card.Img variant="top" src={posterImage} style={{ height: '200px' }} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>Length: {length} minutes</Card.Text>
-        <Card.Text>Categories: {categories.join(', ')}</Card.Text>
-        <Card.Text>Date: {date}</Card.Text>
-      </Card.Body>
+    <Card className="movie-card">
+      <div className="card-content">
+        <div className="poster-container">
+          <Card.Img className="movie-poster" variant="top" src={posterImageURL} />
+        </div>
+        <div className="movie-details">
+          <Card.Body>
+            <Card.Title className="movie-title">{title}</Card.Title>
+            <Card.Text className="movie-info">Length: {length} minutes</Card.Text>
+            <Card.Text className="movie-info">Categories: {categories.join(', ')}</Card.Text>
+            <Card.Text className="movie-info">Date: {date}</Card.Text>
+          </Card.Body>
+        </div>
+      </div>
     </Card>
   );
 };
