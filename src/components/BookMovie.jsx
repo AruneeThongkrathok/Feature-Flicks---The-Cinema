5fa69a6b1f8d5ec6021f1533c13be7320d2a5a5b
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2024-03-11 21:46:04 +0100

    retreived movie info and display it in BookMovie

--- src/components/BookMovie.jsx
+++ src/components/BookMovie.jsx
@@ -1,12 +1,29 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 
 const Bookmovie = () => {
   const { movieId } = useParams();
+  const location = useLocation();
+
+  // Accessing movie details from location state
+  const { movieDetails } = location.state || {};
+  const { id, title, description, selectedCategory } = movieDetails || {};
+  const { categories, posterImage } = description || {};
+
+  if (!id || !title || !description || !selectedCategory) {
+    return <div>No movie details available</div>;
+  }
 
   return (
     <div>
-      <h1>Movie ID: {movieId}</h1>
+      <h1>Movie ID: {id}</h1>
+      <p>Title: {title}</p>
+      <p>Category: {categories}</p>
+      <img
+        src={`https://cinema-rest.nodehill.se/${posterImage}`}
+        alt={title}
+        style={{ maxWidth: "100%" }}
+      />
     </div>
   );
 };
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,51 +1,50 @@
 import React from "react";
 import { Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../css/Movie.css";
 
 const Movie = (props) => {
-  console.log("Movie props:", props);
   const { id, title, description, selectedCategory } = props;
   const { posterImage, length, categories } = description;
   const posterImageURL = "https://cinema-rest.nodehill.se/" + posterImage;
 
+  const navigate = useNavigate();
+
+  const logPropsOnClick = () => {
+    console.log("Clicked Movie props:", props);
+    navigate(`/Bookmovie/${id}`, { state: { movieDetails: props } });
+  };
+
   return (
-    <Link
-      to={{
-        pathname: `/BookMovie/${id}`,
-        state: { movieDetails: props },
-      }}
-    >
-      <Card className="movie-card">
-        <div className="card-content">
-          <div className="poster-container">
-            <Card.Img
-              className="movie-poster"
-              variant="top"
-              src={posterImageURL}
-            />
-          </div>
-          <div className="movie-details">
-            <Card.Body>
-              <Card.Title className="movie-title">{title}</Card.Title>
-              <Card.Text className="movie-info">
-                Categories:{" "}
-                {categories.map((category, index) => (
-                  <span
-                    key={index}
-                    className={`movie-category ${
-                      selectedCategory === category ? "selected" : ""
-                    }`}
-                  >
-                    {category}
-                  </span>
-                ))}
-              </Card.Text>
-            </Card.Body>
-          </div>
+    <Card className="movie-card" onClick={logPropsOnClick}>
+      <div className="card-content">
+        <div className="poster-container">
+          <Card.Img
+            className="movie-poster"
+            variant="top"
+            src={posterImageURL}
+          />
         </div>
-      </Card>
-    </Link>
+        <div className="movie-details">
+          <Card.Body>
+            <Card.Title className="movie-title">{title}</Card.Title>
+            <Card.Text className="movie-info">
+              Categories:{" "}
+              {categories.map((category, index) => (
+                <span
+                  key={index}
+                  className={`movie-category ${
+                    selectedCategory === category ? "selected" : ""
+                  }`}
+                >
+                  {category}
+                </span>
+              ))}
+            </Card.Text>
+          </Card.Body>
+        </div>
+      </div>
+    </Card>
   );
 };
 
