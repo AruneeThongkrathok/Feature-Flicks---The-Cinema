a0524367a55b2ad22ac6abc6c38cce94ea1d021c
Arro <103262516+AruneeThongkrathok@users.noreply.github.com>
2024-03-20 00:18:37 +0100

    imoroved the ui on display of receipt

--- src/components/Receipt.jsx
+++ src/components/Receipt.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import { Card } from "react-bootstrap";
+import "../css/Receipt.css";
 
 function Receipt() {
   const location = useLocation();
@@ -8,27 +9,51 @@ function Receipt() {
     location.state || {};
 
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>Booking Receipt</Card.Title>
-        <div>
-          {" "}
-          {movieTitle && <div>Movie Title: {movieTitle}</div>}
-          {screeningTime && <div>Screening Time: {screeningTime}</div>}
-          {selectedSeats && (
-            <div>
-              Selected Seats:{" "}
-              {selectedSeats
-                .map(
-                  (seat) => `Row: ${seat.rowNumber} Seat: ${seat.seatNumber}`
-                )
-                .join(", ")}
+    <div className="receipt-container">
+      <Card className="receipt-card">
+        <Card.Body className="receipt-card-body">
+          <Card.Title className="receipt-title">
+            Thank you for booking with us!
+          </Card.Title>
+          <Card.Text className="receipt-booking-details">
+            Booking Details:
+          </Card.Text>
+          <div className="receipt-info">
+            {movieTitle && (
+              <div>
+                <strong>Movie Title:</strong> {movieTitle}
+              </div>
+            )}
+            {screeningTime && (
+              <div>
+                <strong>Screening Time:</strong> {screeningTime}
+              </div>
+            )}
+            {selectedSeats && (
+              <div>
+                <strong>Selected Seats:</strong>{" "}
+                {selectedSeats
+                  .map(
+                    (seat) => `Row: ${seat.rowNumber} Seat: ${seat.seatNumber}`
+                  )
+                  .join(", ")}
+              </div>
+            )}
+            <div className="receipt-total-price">
+              <strong>Total Price:</strong> {totalPrice}Kr
             </div>
-          )}
-          <div>Total Price: {totalPrice}Kr</div>
-        </div>
-      </Card.Body>
-    </Card>
+            <div className="receipt-payment-info">
+              Note: Payment required at cinema counter before entry to
+              auditorium.
+            </div>
+            <div className="receipt-greeting-text">
+              We hope to see you again soon!
+            </div>
+            <div className="receipt-greeting-text">Future Flicks Cinema</div>
+          </div>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
